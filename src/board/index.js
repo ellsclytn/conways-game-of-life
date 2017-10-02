85300569d057b59e446a15fd238f795dce9a6e01
Ellis Clayton <ellis@ellisclayton.info>
2017-10-02 15:45:03 +1000

    Add cell lifecycle behaviour

--- src/board/index.js
+++ src/board/index.js
@@ -18,6 +18,58 @@ const createInitialBoard = (
   ))
 )
 
+const liveOrDie = (neighbours, cell) => {
+  if (cell === 1) {
+    if (neighbours < 2) {
+      return 0
+    } else if (neighbours > 1 && neighbours < 4) {
+      return 1
+    } else {
+      return 0
+    }
+  } else {
+    if (neighbours === 3) {
+      return 1
+    } else {
+      return 0
+    }
+  }
+}
+
+const tick = (board) => {
+  const getCell = (x, y) => {
+    const column = board[x] || []
+
+    return column[y] || 0
+  }
+
+  return board.map((column, x) => (
+    column.map((cell, y) => {
+      /* This could definitely be revisited (and I may).
+       * I'm using a hardcoded set here because
+       * the set is small, and fixed, and I'm being lazy. But it's not DRY.
+       * Needs thought, but could use reducers to make this set,
+       * and just exclude the middle entry (self cell)
+       */
+      const neighbours = [
+        getCell(x - 1, y - 1),
+        getCell(x - 1, y),
+        getCell(x - 1, y + 1),
+        getCell(x, y - 1),
+        getCell(x, y + 1),
+        getCell(x + 1, y - 1),
+        getCell(x + 1, y),
+        getCell(x + 1, y + 1)
+      ].reduce((acc, cell) => (
+        acc + cell
+      ), 0)
+
+      return liveOrDie(neighbours, cell)
+    })
+  ))
+}
+
 module.exports = {
-  createInitialBoard
+  createInitialBoard,
+  tick
 }
