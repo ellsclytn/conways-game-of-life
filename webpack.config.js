fe48ebc350b3f6d3fc8cf6a5eb3888e225b52a6f
Ellis Clayton <ellis@ellisclayton.info>
2017-10-02 12:25:56 +1000

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs'
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  }
+}
